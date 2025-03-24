65b435f39de559c472b8de6f448ddabe22052ba3
alexandre bertrand <alexandrebertrand250@gmail.com>
2025-03-24 15:21:13 +0100

    checkpoint befor working on final user interface

---
+++ api_framework/fast-api.py
@@ -0,0 +1,31 @@
+import pandas as pd
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from api_framework.summary_paper import print_sum
+app = FastAPI()
+
+# Allowing all middleware is optional, but good practice for dev purposes
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
+
+@app.get("/predict")
+def predict():
+    """
+    return a summary from an input text file provided by the user
+    """
+
+    df_new = pd.read_excel('raw_data/papers_test.xlsx')
+
+    full_text =df_new["full-text"][0]
+
+    y_pred = print_sum(full_text)
+    return {"Summary": y_pred}
+
+@app.get("/")
+def root():
+    return {'Summarize': "Let's summarize!"}
---
+++ api_framework/test.py
@@ -0,0 +1,3 @@
+import pandas as pd
+df_new = pd.read_excel('raw_data/papers_test.xlsx')
+print(df_new)
